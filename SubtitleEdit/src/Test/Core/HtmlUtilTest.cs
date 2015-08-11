7250b35d6ef8403681115d2110e998430f215766
ikolev <ikolev94@abv.bg>
2015-08-11 09:10:44 +0300

    add HtmlUtil test for all tags

--- SubtitleEdit/src/Test/Core/HtmlUtilTest.cs
+++ SubtitleEdit/src/Test/Core/HtmlUtilTest.cs
@@ -1,23 +1,52 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Nikse.SubtitleEdit.Core;
-
-namespace Test.Core
+﻿namespace Test.Core
 {
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Nikse.SubtitleEdit.Core;
+
     [TestClass]
     public class HtmlUtilTest
     {
         [TestMethod]
         public void TestRemoveOpenCloseTagCyrillicI()
         {
-            const string source = "<\u0456>SubtitleEdit</\u0456>";
-            Assert.AreEqual("SubtitleEdit", HtmlUtil.RemoveOpenCloseTags(source, HtmlUtil.TagCyrillicI));
+            const string Source = "<\u0456>SubtitleEdit</\u0456>";
+            Assert.AreEqual("SubtitleEdit", HtmlUtil.RemoveOpenCloseTags(Source, HtmlUtil.TagCyrillicI));
         }
 
         [TestMethod]
         public void TestRemoveOpenCloseTagFont()
         {
-            const string source = "<font>SubtitleEdit</font>";
-            Assert.AreEqual("SubtitleEdit", HtmlUtil.RemoveOpenCloseTags(source, HtmlUtil.TagFont));
+            const string Source = "<font>SubtitleEdit</font>";
+            Assert.AreEqual("SubtitleEdit", HtmlUtil.RemoveOpenCloseTags(Source, HtmlUtil.TagFont));
+        }
+
+        [TestMethod]
+        public void TestRemoveOpenCloseTagUnderline()
+        {
+            const string Source = "<u>SubtitleEdit</u>";
+            Assert.AreEqual("SubtitleEdit", HtmlUtil.RemoveOpenCloseTags(Source, HtmlUtil.TagUnderline));
+        }
+
+        [TestMethod]
+        public void TestRemoveOpenCloseTagBold()
+        {
+            const string Source = "<b>SubtitleEdit</b>";
+            Assert.AreEqual("SubtitleEdit", HtmlUtil.RemoveOpenCloseTags(Source, HtmlUtil.TagBold));
+        }
+
+        [TestMethod]
+        public void TestRemoveOpenCloseTagParagraph()
+        {
+            const string Source = "<p>SubtitleEdit</p>";
+            Assert.AreEqual("SubtitleEdit", HtmlUtil.RemoveOpenCloseTags(Source, HtmlUtil.TagParagraph));
+        }
+
+        [TestMethod]
+        public void TestRemoveOpenCloseTagItalic()
+        {
+            const string Source = "<i>SubtitleEdit</i>";
+            Assert.AreEqual("SubtitleEdit", HtmlUtil.RemoveOpenCloseTags(Source, HtmlUtil.TagItalic));
         }
     }
 }
