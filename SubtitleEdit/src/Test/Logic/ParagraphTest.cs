f9852070112cf4f70e814325a10b75ff0180b429
ikolev <ikolev94@abv.bg>
2015-08-12 14:47:02 +0300

    finish ParagraphTest.cs (code coverage 90%)

--- SubtitleEdit/src/Test/Logic/ParagraphTest.cs
+++ SubtitleEdit/src/Test/Logic/ParagraphTest.cs
@@ -1,40 +1,62 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Nikse.SubtitleEdit.Logic;
-
-namespace Test.Logic
+﻿namespace Test.Logic
 {
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Nikse.SubtitleEdit.Logic;
+
     [TestClass]
     public class ParagraphTest
     {
         [TestMethod]
         public void TestMethodNumberOfLinesTwoLines()
         {
-            var p = new Paragraph { Text = "Hallo!" + Environment.NewLine + "How are you?" };
-            Assert.AreEqual(2, p.NumberOfLines);
+            var paragraph = new Paragraph { Text = "Hallo!" + Environment.NewLine + "How are you?" };
+            Assert.AreEqual(2, paragraph.NumberOfLines);
         }
 
         [TestMethod]
         public void TestMethodNumberOfLinesThreeLines()
         {
-            var p = new Paragraph { Text = "Hallo!" + Environment.NewLine + "How are you?" + Environment.NewLine + "That's ok."};
-            Assert.AreEqual(3, p.NumberOfLines);            
+            var paragraph = new Paragraph { Text = "Hallo!" + Environment.NewLine + "How are you?" + Environment.NewLine + "That's ok." };
+            Assert.AreEqual(3, paragraph.NumberOfLines);            
         }
 
         [TestMethod]
         public void TestMethodNumberOfLinesOneLine()
         {
-            var p = new Paragraph { Text = "Hallo!" };
-            Assert.AreEqual(1, p.NumberOfLines);
+            var paragraph = new Paragraph { Text = "Hallo!" };
+            Assert.AreEqual(1, paragraph.NumberOfLines);
         }
 
         [TestMethod]
         public void TestMethodNumberOfLinesZero()
         {
-            var p = new Paragraph { Text = string.Empty };
-            Assert.AreEqual(0, p.NumberOfLines);
+            var paragraph = new Paragraph { Text = string.Empty };
+            Assert.AreEqual(0, paragraph.NumberOfLines);
         }
 
+        [TestMethod]
+        public void TestToStringNewParagraph()
+        {
+            string expectedOutput = "00:00:00,000 --> 00:00:00,000 ";
+            var paragraph = new Paragraph();
+            string actualOutput = paragraph.ToString();
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void TestMethodAdjustOneSecond()
+        {
+            var paragraph = new Paragraph();
+            paragraph.Adjust(0, 1);
+            int startSeconds = paragraph.StartTime.Seconds;
+            int endSeconds = paragraph.EndTime.Seconds;
+            Assert.AreEqual(1, startSeconds);
+            Assert.AreEqual(1, endSeconds);
+        }
 
     }
+
 }
