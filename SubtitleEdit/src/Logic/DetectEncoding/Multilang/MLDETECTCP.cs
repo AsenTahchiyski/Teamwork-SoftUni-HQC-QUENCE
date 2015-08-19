58771359e30375adf3b53730e0ad96f587e5191f
ivan <ikolev94@yahoo.bg>
2015-08-19 14:24:35 +0300

    add Multilang/MLDETECTCP.cs

---
+++ SubtitleEdit/src/Logic/DetectEncoding/Multilang/MLDETECTCP.cs
@@ -0,0 +1,23 @@
+﻿namespace Nikse.SubtitleEdit.Logic.DetectEncoding.Multilang
+{
+    public enum Mldetectcp
+    {
+        // Default setting will be used.
+        MldetectcpNone = 0,
+
+        // Input stream consists of 7-bit data.
+        Mldetectcp7Bit = 1,
+
+        // Input stream consists of 8-bit data.
+        Mldetectcp8Bit = 2,
+
+        // Input stream consists of double-byte data.
+        MldetectcpDbcs = 4,
+
+        // Input stream is an HTML page.
+        MldetectcpHtml = 8,
+
+        //Not currently supported.
+        MldetectcpNumber = 16
+    }
+}
